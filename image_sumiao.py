5374caeab45e4648cba8f171533ac23913d7fd1b
kachusung <52709676+kachusung@users.noreply.github.com>
2020-01-24 19:37:08 +0800

    main
    主程序文件

---
+++ image_sumiao.py
@@ -0,0 +1,27 @@
+import cv2
+# 输入图片路径
+SRC = "image_name"
+img_rgb = cv2.imread(SRC)
+
+# 原图
+# cv2.imshow('rbg', img_rbg)
+# cv2.waitKey(0)
+# exit()
+
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+# 展示灰度图
+# cv2.imshow("gray", img_gray)
+# cv2.waitKey(0)
+# exit()
+
+img_blur = cv2.GaussianBlur(img_gray, ksize=(21, 21), sigmaX=0, sigmaY=0)
+# 展示高斯虚化后效果图
+# cv2.imshow("img_blur", img_blur)
+# cv2.waitKey(0)
+# exit()
+
+img_blend = cv2.divide(img_gray, img_blur, scale=255)
+# 展示素描图
+cv2.imshow("img_blend", img_blend)
+cv2.waitKey(0)
+cv2.imwrite("image_sumiao.jpg", img_blend)
